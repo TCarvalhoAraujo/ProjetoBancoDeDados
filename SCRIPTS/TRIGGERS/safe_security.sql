75b74897f40319069a3fa9a9c572ba2af044a9e4
Thiago Carvalho <thicoaraujo1@gmail.com>
2024-11-18 21:22:41 -0500

    Update safe_security.sql

--- SCRIPTS/TRIGGERS/safe_security.sql
+++ SCRIPTS/TRIGGERS/safe_security.sql
@@ -1,17 +1,15 @@
 DELIMITER $$
-
 CREATE TRIGGER safe_security
 BEFORE UPDATE ON ARQUIVO
 FOR EACH ROW
 BEGIN
-    IF LOWER(NEW.TIPO) = 'exe' 
-        OR LOWER(NEW.TIPO) = 'bat'
-        OR LOWER(NEW.TIPO) = 'sh'
-        OR LOWER(NEW.TIPO) = 'com'
-        OR LOWER(NEW.TIPO) = 'cmd' THEN
+    IF LOWER(NEW.TIPO) = '.exe' 
+        OR LOWER(NEW.TIPO) = '.bat'
+        OR LOWER(NEW.TIPO) = '.sh'
+        OR LOWER(NEW.TIPO) = '.com'
+        OR LOWER(NEW.TIPO) = '.cmd' THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Arquivos executáveis não são permitidos.';
     END IF;
 END $$
-
 DELIMITER ;
